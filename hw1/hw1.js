ed0242a31246cac7eacf82c20e21b3c790360a35
unknown <wer4ty@gmail.com>
2018-10-30 14:41:58 +0200

    Init

---
+++ hw1/hw1.js
@@ -0,0 +1,13 @@
+
+function cicleArea1(r) {
+	let PI = 3.14;
+	return PI * r *r;
+}
+
+let cicleArea2 = (r) =>  {
+	let PI = 3.14;
+	return PI * r *r;
+}
+
+console.log(cicleArea1(7));
+console.log(cicleArea2(7));
