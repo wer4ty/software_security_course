e9e7166eb1c329fcaecf0f528e023473541a3385
wer4ty <roma0152@bitbucket.org>
2018-10-31 13:21:34 +0200

    Rotor design

--- hw1/enigma/enigma.js
+++ hw1/enigma/enigma.js
@@ -5,11 +5,12 @@
 
 class Rotor {
 
-	constructor(p, ind, tn){
-		this.position = p;
-		this.index = ind;
-		this.forward_permutation = this.mixing();
-		this.turnover_notch = tn;
+	constructor(p, ind, fd, rv, tn){
+		this.position = p; // Left, Middle, Right
+		this.index = ind; // 01 - 26
+		this.forward_permutation = fd; // ABCDEFGHIJKLMNOPQRSTVWXYZ
+		this.reverse_permutation = rv; // ZYXWVTSRQPONMLKJIHGFEDCBA
+		this.turnover_notch = tn; // Q -> R
 	}
 
 
@@ -25,14 +26,20 @@ class Rotor {
 		return array;
 	}
 
-	getPosition() { console.log(this.position); }
-	getIndex() { console.log(this.index); }
-	getForwardPermutation() { console.log(this.forward_permutation); }
-	getTurnoverNotch() { console.log(this.turnover_notch); }
+	getPosition() { console.log(this.position); return this.position; }
+	getIndex() { console.log(this.index); return this.index }
+	getForwardPermutation() { console.log(this.forward_permutation); return this.forward_permutation; }
+	getReverscePermutation() { console.log(this.reverse_permutation); return this.reverse_permutation; }
+	getTurnoverNotch() { console.log(this.turnover_notch); return this.turnover_notch; }
+	incrementIndex() { this.index = (this.index + 1) % 26; }
 }
 
-let I = new Rotor("L", 1, "QR");
-I.getPosition();
-I.getIndex();
-I.getForwardPermutation();
-I.getTurnoverNotch();
+
+class Enigma {
+
+	constructor(r1,r2,r3) {
+		this.leftRotor = r1;
+		this.middleRotor = r2;
+		this.rightRotor = r3;
+	}
+}
