f9a55c468bb1de1f23e0004051a46b7505a5220f
wer4ty <roma0152@bitbucket.org>
2018-12-06 21:43:56 +0200

    scapy

--- hw3/client.py
+++ hw3/client.py
@@ -2,22 +2,36 @@ import socket
 import time
 
 from scapy.all import *
-host="127.0.0.1"
-message="A"*496+"B"*500
-packet=IP(dst=host,id=12345)/UDP(sport=12321,dport=12321)/message
- 
-frags=fragment(packet,fragsize=100)
- 
-counter=1
-for fragment in frags:
-  print "Packet no#"+str(counter)
-  print "==================================================="
-  fragment.show() #displays each fragment
-  counter+=1
-  send(fragment)
-  time.sleep(3)
 
-# from scapy.all import *
+# set interface
+conf.iface="lo0"
+
+# create IP packet
+ip_pkt = IP()/UDP(dport=12321)
+ip_pkt.payload = "payload test message"
+#ip_pkt.dport = 12321
+ip_pkt.dst = "127.0.0.1"
+ip_pkt.src = "127.0.0.1"
+
+# send out packet
+send(ip_pkt)
+
+
+#host="127.0.0.1"
+#message="A"*496+"B"*500
+#packet=IP(dst=host,id=12345)/UDP(sport=12300,dport=12321)/message
+ 
+#frags=fragment(packet,fragsize=100)
+ 
+#counter=1
+#for fragment in frags:
+#  print "Packet no#"+str(counter)
+  #print "==================================================="
+  #fragment.show() #displays each fragment
+#  counter+=1
+#  sr(fragment)
+#  time.sleep(3)
+
 
 # server_address = ('127.0.0.1', 12321)
 # message = 'This is the message.  It will be repeated.'
--- hw3/serv.py
+++ hw3/serv.py
@@ -1,14 +1,17 @@
 import socket
+from scapy.all import *
+
+
 
 HOST = '127.0.0.1'
 PORT = 12321
 
 while True:
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind((HOST, PORT))
-	data, addr = s.recvfrom(1024)
-	print (addr)
-	print (data)
-	data += "CR/LF"
-	sent = s.sendto(data, addr)
+ 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ 	s.bind((HOST, PORT))
+ 	data, addr = s.recvfrom(100)
+ 	print (addr)
+ 	print (data)
+ 	data += "CR/LF"
+ 	sent = s.sendto(data, addr)
 s.close()
