1a77832790a7d8135dfe13459f3345878133fd2d
wer4ty <roma0152@bitbucket.org>
2018-12-06 20:49:18 +0200

    scapy send packat fixed size 100 bits

--- hw3/client.py
+++ hw3/client.py
@@ -1,17 +1,35 @@
 import socket
 import time
 
-server_address = ('127.0.0.1', 12321)
-message = 'This is the message.  It will be repeated.'
-i = 0
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+from scapy.all import *
+host="127.0.0.1"
+message="A"*496+"B"*500
+packet=IP(dst=host,id=12345)/UDP(sport=12321,dport=12321)/message
+ 
+frags=fragment(packet,fragsize=100)
+ 
+counter=1
+for fragment in frags:
+  print "Packet no#"+str(counter)
+  print "==================================================="
+  fragment.show() #displays each fragment
+  counter+=1
+  send(fragment)
+  time.sleep(3)
 
-while True:
-	i+=1
-	message2 = str(i)+" "+message
-	time.sleep(3)
-	sent = s.sendto(message2, server_address)
-	data, server = s.recvfrom(1024)
-	print(data)
+# from scapy.all import *
 
-s.close()
+# server_address = ('127.0.0.1', 12321)
+# message = 'This is the message.  It will be repeated.'
+# i = 0
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# while True:
+# 	i+=1
+# 	message2 = str(i)+" "+message
+# 	time.sleep(3)
+# 	sent = s.sendto(message2, server_address)
+# 	data, server = s.recvfrom(1024)
+# 	print(data)
+
+# s.close()
